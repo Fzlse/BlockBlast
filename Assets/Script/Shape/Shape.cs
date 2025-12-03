6c7962268539075f9545fb1be298039d34724b4f
Muhammad Andra Ramadhani <146610061+Fzlse@users.noreply.github.com>
2025-12-03 21:29:40 +0700

    Add drag-and-drop for shapes and shape storage
    Introduced drag-and-drop interaction to Shape.cs using Unity event interfaces, allowing shapes to be moved and scaled on drag. Added ShapeStorage.cs to manage and randomly assign shape data to shape instances. Updated prefabs and Game.unity scene to support new shape management and interaction features, including physics components for SquareImage.

--- Assets/Script/Shape/Shape.cs
+++ Assets/Script/Shape/Shape.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Shape : MonoBehaviour
+public class Shape : MonoBehaviour, IPointerClickHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
     public GameObject squareShapeImagaePrefab;
+    public Vector3 shapeSelectedoScale;
+    public Vector2 offset = new Vector2(0f, 700f);
 
-    //[HideInInspector]
+    [HideInInspector]
     public ShapeData CurrentShapeData;
     private List<GameObject> _currentShapeSquares = new List<GameObject>();
+    private Vector3 _shapeStartScale;
+    private RectTransform _transform;
+    private bool shapeDragged = true;
+    private Canvas _canvas;
+
+    public void Awake()
+    {
+        _shapeStartScale = this.GetComponent<RectTransform>().localScale;
+        _transform = this.GetComponent<RectTransform>();
+        _canvas = GetComponentInParent<Canvas>();
+        shapeDragged = true;
+    }
 
     void Start()
      {
-        RequestNewShape(CurrentShapeData);
+        
      }
 
     public void RequestNewShape(ShapeData shapeData)
@@ -108,4 +123,40 @@ public class Shape : MonoBehaviour
         }
         return number;
     }
+   public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        this.GetComponent<RectTransform>().localScale = shapeSelectedoScale;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _transform.anchorMin = new Vector2(0, 0);
+        _transform.anchorMax = new Vector2(0, 0);
+        _transform.pivot = new Vector2(0, 0);
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _canvas.transform as RectTransform,
+            eventData.position,
+            Camera.main,
+            out pos);
+        _transform.localPosition = pos + offset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.GetComponent<RectTransform>().localScale = _shapeStartScale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    } 
 }
